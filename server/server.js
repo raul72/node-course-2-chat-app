81434640ad6cf32a836707aa8069a671506a44ff
Raul Raat <raulraat@gmail.com>
2018-10-13 17:16:41 +0300

    socket.io connect/disconnect events

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const config = require('./config/config.js');
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(config.port, () => {
+io.on('connection', (socket) => {
+
+  console.log('connected with a client');
+
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
+  });
+
+});
+
+server.listen(config.port, () => {
   console.log(`Server is up on port ${config.port}`);
 });

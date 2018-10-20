5426b1b3e20828e434d7dd02853bcf4a75d6af16
Raul Raat <raul.raat@delfi.ee>
2018-10-20 14:27:55 +0300

    UI with jQuery

--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
-var socket = io();
+jQuery(function($){
+  var socket = io();
 
-socket.on('connect', () => {
-  console.log('connected to the server');
-});
+  socket.on('connect', () => {
+    console.log('connected to the server');
+  });
 
-socket.on('disconnect', () => {
-  console.log('disconnected from the server');
-});
+  socket.on('disconnect', () => {
+    console.log('disconnected from the server');
+  });
 
-socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
-});
+  const $messages = $('#messages');
+  socket.on('newMessage', function (message) {
+    $('<li/>')
+      .text(`${message.from}: ${message.text}`)
+      .appendTo($messages)
+    ;
+    console.log('newMessage', message);
+  });
 
-socket.emit('createMessage', {
-  from: 'Bob',
-  text: 'Bob was here'
-}, function(payload){
-  console.log('Got it', payload);
+  socket.emit('createMessage', {
+    from: 'Bob',
+    text: 'Bob was here'
+  }, function(payload){
+    console.log('Got it', payload);
+  });
+
+  const $form = $('#message-form');
+  const $text = $('input[name="message"]', $form);
+  $form.on('submit', function(e){
+    $text.attr('disabled', true);
+    socket.emit('createMessage', {
+      from: 'Bob',
+      text: $text.val()
+    }, function(payload){
+      $text.val('');
+      $text.attr('disabled', false);
+      $text.focus();
+      console.log('Got it', payload);
+    });
+    return false;
+  });
+
 });

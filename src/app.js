9747c66b375ed8e3476ee69d73322d762291dd39
Raul Raat <raul.raat@delfi.ee>
2018-12-08 11:24:32 +0200

    styles and ux

--- src/app.js
+++ src/app.js
@@ -55,13 +55,27 @@ jQuery(function($){
       return alert('Geolocation not supported by your browser');
     }
 
+    $locationButton
+      .attr('disabled', true)
+      .text('Sending location...')
+    ;
+
     navigator.geolocation.getCurrentPosition(function(position){
-      console.log(position);
+
+      $locationButton
+        .removeAttr('disabled')
+        .text('Send location')
+      ;
+
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     }, function(){
+      $locationButton
+        .removeAttr('disabled')
+        .text('Send location')
+      ;
       alert('Unable to fetch location.');
     }, {
 
